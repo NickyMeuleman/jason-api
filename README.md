93a4bf5c27bb17ce2217008c04ef3240b0b96509
Nicky Meuleman <nicky.meuleman@gmail.com>
2019-08-23 17:55:46 +0200

    init graphql

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # JaSON API
 
 Too many Jasons?
+
+## dev notes
+
+- add prettier and eslint
+- strict things up in tsconfig again
+- type things in the resolver
+- replace placeholder database
+- alloz unused function parameters, but not other vars
---
+++ src/lambda/graphql.ts
@@ -0,0 +1,65 @@
+import { Handler } from "aws-lambda";
+import { ApolloServer, gql } from "apollo-server-lambda";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+    allAuthors: [Author!]
+    author(id: Int!): Author
+    authorByName(name: String!): Author
+  }
+  type Author {
+    id: ID!
+    name: String!
+    married: Boolean!
+  }
+`;
+
+interface Author {
+  id: number;
+  name: string;
+  married: boolean;
+}
+
+const authors: Author[] = [
+  { id: 1, name: "Terry Pratchett", married: false },
+  { id: 2, name: "Stephen King", married: true },
+  { id: 3, name: "JK Rowling", married: false }
+];
+
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => {
+      return "Hello, world!";
+    },
+    allAuthors: (root, args, context) => {
+      return context.db.authors;
+    },
+    author: (root, args, context) => {
+      return;
+    },
+    authorByName: (root, args, context) => {
+      console.log("hihhihi", args.name);
+      return authors.find(x => x.name === args.name) || "NOTFOUND";
+    }
+  }
+};
+
+interface MyContext {
+  db: Author[];
+}
+
+const server: ApolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }): MyContext => ({
+    ...req,
+    db: authors
+  }),
+  playground: true,
+  introspection: true
+});
+
+const handler: Handler = server.createHandler();
+
+export { handler };
